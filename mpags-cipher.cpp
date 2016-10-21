1159c683ead32a30b6b8ecbf3697b90b864230e2
James Pillow <j.pillow@warwick.ac.uk>
2016-10-21 13:19:45 +0100

    Second exercise, int and double

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -4,7 +4,25 @@ int main()
 	// This is a comment
 	/* This is a 
 	Multiline comment */
+
+	//std::cout << "Hello World!\n";
 	
-	std::cout << "Hello World!\n";
+	int a{9};
+	int b{124};
+	int c{1};
+	
+	c = a + b;
+	
+	std::cout << c << std::endl;
+	
+	double d{142.6};
+	
+	std::cout << d << std::endl;
+	
+	int h{d};
+	
+	std::cout << h << std::endl;
+	
+	return 0;
 	
 }
